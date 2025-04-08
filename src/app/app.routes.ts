f5082c7454496a75a92287338f993fb22adc6ee6
antoniozamora2002 <antoniozp02@gmail.com>
2025-04-08 11:40:44 -0500

    first commit by the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PruebasComponent } from './components/pruebas/pruebas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'pruebas',
+    pathMatch: 'full',
+  },
+  {
+    path: 'pruebas',
+    component: PruebasComponent,
+  },
+];
